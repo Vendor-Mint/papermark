e9da7866cb8e5a93d08e2cda71579367a15ecc65
oscar <oscar@oscar.com>
2025-05-27 11:22:28 -0400

    fix

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'papermark',
+      script: 'npm',
+      args: 'run dev',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+      },
+      watch: true,
+      autorestart: true,
+      max_memory_restart: '2G',
+      instances: 1,
+      exec_mode: 'fork',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      error_file: 'logs/error.log',
+      out_file: 'logs/out.log',
+      time: true
+    }
+  ]
+}
